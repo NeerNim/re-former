c0b39a0e367487f28bb23da4a077d7e472281e66
whiz25 <afanimartin@gmail.com>
2019-07-08 15:24:06 +0300

    Add edit functionality for an existing user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only:[:create, :new]
-
+  resources :users, only:[:create, :new, :edit, :update]
+  # put "user/:id/update" => "user#update", as: "update_user"
 end
