19d1f62f4da010a04fbe13ac1f1970f9dfc9665b
NeerNim <neernimt@gmail.com>
2019-07-08 13:02:29 +0200

    create user in database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
-  def new 
+  def new
+    @user = User.new
   end
-  
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 
 end
